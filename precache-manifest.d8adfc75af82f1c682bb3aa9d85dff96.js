cd771e3ab27b8aa98df197f0eaf536771433b207
thepuzzletechie <w3.author@gmail.com>
2020-07-05 15:50:31 +0530

    Updates

---
+++ precache-manifest.d8adfc75af82f1c682bb3aa9d85dff96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e313e55a3bff4a84f5032fc2897b1851",
+    "url": "/meal/index.html"
+  },
+  {
+    "revision": "e1cce0f06c2daf10e2e7",
+    "url": "/meal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bea8b281c9122efe8e1d",
+    "url": "/meal/static/js/2.b5d88dcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meal/static/js/2.b5d88dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cce0f06c2daf10e2e7",
+    "url": "/meal/static/js/main.ac835434.chunk.js"
+  },
+  {
+    "revision": "bf9444f3a6debcc88e73",
+    "url": "/meal/static/js/runtime-main.df7e14a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meal/static/media/logo.5d5d9eef.svg"
+  }
+]);
